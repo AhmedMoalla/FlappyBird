28538ac45361bae50498b3a92d0012211b53d5bc
Ahmed Moalla <ahmed.moalla@novencia.com>
2025-07-10 12:17:48 +0200

    removed circular dependency issues and exported web version

--- scripts/menu_button.gd
+++ scripts/menu_button.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-@export var menu_scene: PackedScene
+var menu_scene: PackedScene = load("res://scenes/menu.tscn")
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_packed(menu_scene)
--- scripts/start.gd
+++ scripts/start.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-@export var game_scene: PackedScene
+var game_scene: PackedScene = load("res://scenes/main.tscn")
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
